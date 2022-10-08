927a917065c16dc75d7ad9092db6a18d54843e9d
Parkson Tano Daniel <tanoparksonsilencer@gmail.com>
2022-10-08 13:26:49 +0100

    Update urls.py

--- darkweb/urls.py
+++ darkweb/urls.py
@@ -6,7 +6,7 @@ from django.urls import include, re_path
 from django.views.static import serve
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin-sha-rjah/', admin.site.urls),
     path('', include('main.urls')),
     re_path(r'^media/(?P<path>.*)$', serve,
             {'document_root': settings.MEDIA_ROOT}),
