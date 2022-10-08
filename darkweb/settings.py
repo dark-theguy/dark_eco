5a3269fa5f26e5a7e6d1b447abd6af833d27132b
parkson-tano <tanoparksonsilencer@gmail.com>
2022-10-08 03:21:25 +0100

    fix modal

--- darkweb/settings.py
+++ darkweb/settings.py
@@ -121,18 +121,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-
-
-if DEBUG:
-    STATICFILES_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+]
 
-    
+
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
