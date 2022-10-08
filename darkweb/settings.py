0554b308110530f7ee67bd28c625bbff618a4df1
Parkson Tano Daniel <tanoparksonsilencer@gmail.com>
2022-10-08 13:01:39 +0100

    Update settings.py

--- darkweb/settings.py
+++ darkweb/settings.py
@@ -139,7 +139,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://appname.herokuapp.com'
+    'https://sharjah-pharma.com'
 ]
 
 # SECURE_SSL_REDIRECT = True
